0abf5efb5074d5c8fac5535f6c9a260f3016042a
Magic-Mayo <mike.mayoii@gmail.com>
2019-12-20 02:55:04 -0700

    added useState for handling hover events. used props for filling in some content and handling click events

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Github from '../Logos/Github';
 
-export default function Projects(){
+export default function Projects(props){
+
+    const [opacity, setOpacity] = useState(.3);
+
     return (
-        <div>
-            Projects
+        <div className={`project project-${props.github}`} onMouseEnter={()=>setOpacity(1)} onMouseLeave={()=>setOpacity(.3)} style={{opacity: `${opacity}`}}>
+            <h1 className={`project-title project-title-${props.name}`} onClick={()=>window.open(`${props.address}`, '_blank','')}>{props.name}</h1>
+            <Github address={props.github} />
+            <p>{props.desc}</p>
+            <img src={process.env.PUBLIC_URL + props.img} alt={"name"} className={`img img-${props.github}`} onClick={()=>window.open(`${props.address}`, '_blank','')}></img>
         </div>
     )
 }
