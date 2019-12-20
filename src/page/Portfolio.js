e3952b7b41bceb07052e9132869c322170e7a837
Magic-Mayo <mike.mayoii@gmail.com>
2019-12-20 02:55:04 -0700

    mapped out projects component from json data

--- src/page/Portfolio.js
+++ src/page/Portfolio.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import About from '../components/About';
+// import About from '../components/About';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
+import projectData from '../data/projects.json'
 
 export default function Portfolio(){
     return (
@@ -10,7 +11,18 @@ export default function Portfolio(){
                 <span className='app-myName'>Mike Mayo</span>
                 <span className='app-descriptor'>Thinker | Creator | Developer</span>
             </header>
-            <About />
+            {/* <About /> */}
+            {projectData.map(project=>{
+                console.log(project)
+                return <Projects
+                key={project.name}
+                name={project.name}
+                address={project.address}
+                img={project.img}
+                github={project.github}
+                desc={project.description}
+                />
+            })}
             <Projects />
             <Contact />
         </div>
