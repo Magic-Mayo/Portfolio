e0d0c2d6d0d4cb3c93f533d870c90ba61228b74b
Magic-Mayo <mike.mayoii@gmail.com>
2019-12-20 20:13:00 -0700

    created hook for img span. created effect to limit when the img span will be rendered

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,16 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Github from '../Logos/Github';
 
 export default function Projects(props){
 
     const [opacity, setOpacity] = useState(.3);
+    const [hoverText, setHoverText] = useState(false);
+
+    useEffect(()=>{
+        setHoverText(hoverText)
+    },[hoverText])
 
     return (
         <div className={`project project-${props.github}`} onMouseEnter={()=>setOpacity(1)} onMouseLeave={()=>setOpacity(.3)} style={{opacity: `${opacity}`}}>
             <h1 className={`project-title project-title-${props.name}`} onClick={()=>window.open(`${props.address}`, '_blank','')}>{props.name}</h1>
             <Github address={props.github} />
             <p>{props.desc}</p>
-            <img src={process.env.PUBLIC_URL + props.img} alt={"name"} className={`img img-${props.github}`} onClick={()=>window.open(`${props.address}`, '_blank','')}></img>
+            <div className='img-container' onClick={()=>window.open(`${props.address}`, '_blank','')} onMouseEnter={()=>setHoverText(true)} onMouseLeave={()=>setHoverText(false)}>
+                <img src={process.env.PUBLIC_URL + props.img} style={{}} alt={"name"}  className={`img img-${props.github}`}></img>
+                {hoverText && <span>Click to visit site</span>}
+            </div>
         </div>
     )
 }
