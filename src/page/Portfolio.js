ad608052a1b9c5c50ad528089b8a4c46293b9d88
Magic-Mayo <mike.mayoii@gmail.com>
2019-12-26 22:20:32 -0700

    uncommented about component and added wrapper and header for projects components. added gif prop to projects

--- src/page/Portfolio.js
+++ src/page/Portfolio.js
@@ -1,30 +1,28 @@
 import React from 'react';
-// import About from '../components/About';
+import About from '../components/About';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 import projectData from '../data/projects.json'
 
 export default function Portfolio(){
     return (
-        <div className='app'>
-            <header className='app-header'>
-                <span className='app-myName'>Mike Mayo</span>
-                <span className='app-descriptor'>Thinker | Creator | Developer</span>
-            </header>
-            {/* <About /> */}
-            {projectData.map(project=>{
-                console.log(project)
-                return <Projects
-                key={project.name}
-                name={project.name}
-                address={project.address}
-                img={project.img}
-                github={project.github}
-                desc={project.description}
-                />
-            })}
-            <Projects />
+        <>
+            <About />
+            <div className='project-container'>
+                <h1>Projects</h1>
+                {projectData.map(project=>{
+                    return <Projects
+                    key={project.name}
+                    name={project.name}
+                    address={project.address}
+                    img={project.img}
+                    gif={project.gif}
+                    github={project.github}
+                    desc={project.description}
+                    />
+                })}
+            </div>
             <Contact />
-        </div>
+        </>
     )
 }
