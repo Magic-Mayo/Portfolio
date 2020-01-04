542a5177e9f7f6b58071a45a271112e24b009c1c
Magic-Mayo <mike.mayoii@gmail.com>
2020-01-03 19:42:06 -0700

    removed header and created a separate component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import Portfolio from './page/Portfolio';
-import { useInView } from 'react-intersection-observer';
+import Header from './components/Header';
 
-let timeout = setTimeout(()=>'inview',1000);
 export default function App() {
-
-    const [ref, inView] = useInView({
-        threshold: .99
-    })
-
-    useEffect(()=>{
-        setTimeout(()=>{
-            return timeout === 'inview' ? 'outview' : 'inview'
-        }, 1010)
-    },[inView])
 
     return (
         <div className='app'>
-            <header className='app-header' ref={ref}>
-                
-                <span
-                className={`app-myName ${inView ? 'inview' : 'outview'}`}>
-                    Mike Mayo
-                </span>
-                
-                <span
-                className={`app-descriptor ${inView ? 'inview' : 'outview'}`}
-                style={{visibility: timeout === 'inview' ? 'visible' : 'hidden'}}>
-                    Thinker | Creator | Developer
-                </span>
-            </header>
+            <Header />
             <Portfolio />
         </div>
     );
