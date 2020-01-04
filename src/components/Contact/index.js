cf125129a776d5f6b960899506d9cf92b1455dcf
Magic-Mayo <mike.mayoii@gmail.com>
2020-01-03 19:42:06 -0700

    created 2 inputs and a textarea for contacts name email and message to send. created hooks to handle input change and reset on form submission

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Contact(){
+    const [contactName, setContactName] = useState('');
+    const [contactEmail, setContactEmail] = useState('');
+    const [contactMessage, setContactMessage] = useState('');
+
     return(
-        <div>
-            Contact
+        <div className='contact-form-wrapper'>
+        <span className='contact-form-header'>Contact Mike Mayo</span>
+            <form className='contact-form' netlify>
+                <input
+                className='contact-form-name'
+                placeholder='Preferred contact name'
+                onChange={(e)=>setContactName(e.target.value)}
+                value={contactName}/>
+                <input
+                className='contact-form-email'
+                placeholder='Email address'
+                onChange={(e)=>setContactEmail(e.target.value)}
+                value={contactEmail}/>
+                <textarea
+                className='contact-form-message'
+                placeholder='Enter message to send to Mike Mayo here!'
+                onChange={(e)=>setContactMessage(e.target.value)}
+                value={contactMessage}>
+                </textarea>
+                <button
+                className='contact-form-submit'
+                onClick={(e)=>{
+                    e.preventDefault();
+                    setContactMessage('');
+                    setContactName('');
+                    setContactEmail('');}}>Submit</button>
+            </form>
         </div>
     )
 }
