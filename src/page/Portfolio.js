377f564ed506f0eddb1d4ee9fbc48f0ea4d4f05b
Magic-Mayo <mike.mayoii@gmail.com>
2020-01-07 14:54:32 -0700

    mapped out sets of projects instead of all at once

--- src/page/Portfolio.js
+++ src/page/Portfolio.js
@@ -8,19 +8,47 @@ export default function Portfolio(){
     return (
         <>
             <About />
-            <section className='project-container'>
+            <section className='project-wrapper'>
                 <h1 className='project-header'>Projects</h1>
-                {projectData.map(project=>{
-                    return <Projects
-                    key={project.name}
-                    name={project.name}
-                    address={project.address}
-                    img={project.img}
-                    gif={project.gif}
-                    github={project.github}
-                    desc={project.description}
-                    />
-                })}
+                <span className='project-container'>
+                    {projectData[0].map(project=>{
+                        return <Projects
+                        key={project.name}
+                        name={project.name}
+                        address={project.address}
+                        img={project.img}
+                        gif={project.gif}
+                        github={project.github}
+                        desc={project.description}
+                        />
+                    })}
+                </span>
+                <span className='project-container'>
+                    {projectData[1].map(project=>{
+                        return <Projects
+                        key={project.name}
+                        name={project.name}
+                        address={project.address}
+                        img={project.img}
+                        gif={project.gif}
+                        github={project.github}
+                        desc={project.description}
+                        />
+                    })}
+                </span>
+                <span className='project-container'>
+                    {projectData[2].map(project=>{
+                        return <Projects
+                        key={project.name}
+                        name={project.name}
+                        address={project.address}
+                        img={project.img}
+                        gif={project.gif}
+                        github={project.github}
+                        desc={project.description}
+                        />
+                    })}
+                </span>
             </section>
             <Contact />
         </>
