4b88f887967661d0ac25778e6ac5f7d1c7f2561d
Magic-Mayo <mike.mayoii@gmail.com>
2020-01-06 23:14:49 -0700

    added span around inputs so they will be on the same line. changed button text to send instead of submit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -31,31 +31,33 @@ export default function Contact(){
 
     return(
         <div className='contact-form-wrapper'>
-        <span className='contact-form-header'>Contact Mike Mayo</span>
+            <span className='contact-form-header'>Contact Mike Mayo</span>
             <form
             className='contact-form'
             netlify='true'
             name='messages'
             onSubmit={handleSubmit}>
-                <input
-                className='contact-form-name'
-                placeholder='Preferred contact name'
-                onChange={(e)=>setContactName(e.target.value)}
-                value={contactName}
-                type='text'/>
-                <input
-                className='contact-form-email'
-                placeholder='Email address'
-                onChange={(e)=>setContactEmail(e.target.value)}
-                value={contactEmail}
-                type='email'/>
+                <span>
+                    <input
+                    className='contact-form-name'
+                    placeholder='Preferred contact name'
+                    onChange={(e)=>setContactName(e.target.value)}
+                    value={contactName}
+                    type='text'/>
+                    <input
+                    className='contact-form-email'
+                    placeholder='Email address'
+                    onChange={(e)=>setContactEmail(e.target.value)}
+                    value={contactEmail}
+                    type='email'/>
+                </span>
                 <textarea
                 className='contact-form-message'
                 placeholder='Enter message to send to Mike Mayo here!'
                 onChange={(e)=>setContactMessage(e.target.value)}
                 value={contactMessage}>
                 </textarea>
-                <button className='contact-form-submit'>Submit</button>
+                <button className='contact-form-submit'>Send!</button>
             </form>
         </div>
     )
