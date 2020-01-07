2af935b4a9a81ca5ec24387c407b4e5ee8a3fa5b
Magic-Mayo <mike.mayoii@gmail.com>
2020-01-06 21:49:56 -0700

    set new useState functions to be used by useLayoutEffect to set state at a time after render. set class names based on those changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 export default function Header(props){
 
-    const [titleRef, titleView] = useInView({
-        threshold: .99
-    });
+    const [titleRef, titleView] = useInView({threshold: .75});
+    const [thinkerView, setThinkerView] = useState(false);
+    const [creatorView, setCreatorView] = useState(false);
+    const [developerView, setDeveloperView] = useState(false);
+    const [separatorView, setSeparatorView] = useState(false);
 
-    let viewTimeout;
-
-    useEffect(()=>{
+    useLayoutEffect(()=>{
         setTimeout(() => {
-            return viewTimeout = titleView;
+            return setThinkerView(true);
         }, 1000);
+        setTimeout(() => {
+            return setCreatorView(true);
+        }, 1450);
+        setTimeout(() => {
+            return setDeveloperView(true);
+        }, 1900);
+        setTimeout(() => {
+            return setSeparatorView(true);
+        }, 2350);
 
-        return () => viewTimeout;
-    }, [titleView])
+        return ()=>{
+            setThinkerView(false);
+            setCreatorView(false);
+            setDeveloperView(false);
+            setSeparatorView(false);
+        }
+    },[titleView])
+
     return (
         <header className='app-header' ref={titleRef}>
                 
@@ -26,27 +41,27 @@ export default function Header(props){
                 
                 <div className='app-descriptor'>
                     <span
-                    className={`app-descriptor-thinker app-descriptor ${viewTimeout ? 'inview' : 'outview'}`}>
+                    className={`app-descriptor-thinker app-descriptor ${!thinkerView ? 'outview' : 'inview'} ${titleView ? '' : 'hidden'}`}>
                         Thinker&nbsp;
                     </span>
 
                     <span
-                    className={`app-descriptor-separator app-descriptor ${!titleView ? 'outview' : 'inview'}`}>
+                    className={`app-descriptor-separator app-descriptor ${separatorView ? 'inview' : 'outview'} ${titleView ? '' : 'hidden'}`}>
                         |
                     </span>
 
                     <span
-                    className={`app-descriptor-creator app-descriptor ${!titleView ? 'outview' : 'inview'}`}>
+                    className={`app-descriptor-creator app-descriptor ${creatorView ? 'inview' : 'outview'} ${titleView ? '' : 'hidden'}`}>
                         &nbsp;Creator&nbsp;
                     </span>
 
                     <span
-                    className={`app-descriptor-separator app-descriptor ${!titleView ? 'outview' : 'inview'}`}>
+                    className={`app-descriptor-separator app-descriptor ${separatorView ? 'inview' : 'outview'} ${titleView ? '' : 'hidden'}`}>
                         |
                     </span>
 
                     <span
-                    className={`app-descriptor-developer app-descriptor ${!titleView ? 'outview' : 'inview'}`}>
+                    className={`app-descriptor-developer app-descriptor ${developerView ? 'inview' : 'outview'} ${titleView ? '' : 'hidden'}`}>
                         &nbsp;Developer
                     </span>
                 </div>
