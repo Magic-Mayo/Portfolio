098af9f55ceca83dc66510dc762acd6ceff9ee59
Magic-Mayo <mike.mayoii@gmail.com>
2020-01-08 18:25:22 -0700

    added second shake hook for better control and changed around timeouts in useEffect. sent in props for logos so it knows if it's going to be on the nav bar.formatted

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
+import Logo from '../Logos'
 
 export default function Header(props){
 
-    const [titleRef, titleView] = useInView({rootMargin: '-450px 0px 0px 0px'});
+    const [titleRef, titleView] = useInView({rootMargin: '-81% 0px 0px 0px'});
     const [thinkerView, setThinkerView] = useState(false);
     const [creatorView, setCreatorView] = useState(false);
     const [developerView, setDeveloperView] = useState(false);
     const [shake, setShake] = useState(false);
+    const [shook, setShook] = useState(false);
 
     useEffect(()=>{
         setTimeout(() => {
@@ -23,30 +25,39 @@ export default function Header(props){
             return setShake(true)
         }, 1200)
         setTimeout(()=>{
-            return setShake(false)
-        }, 1270)
-        setTimeout(()=>{
-            return setShake(true)
+            return setShook(true)
         }, 1500)
-        setTimeout(()=>{
-            return setShake(false)
-        }, 1570)
     },[titleView])
 
     return (
         <>
-            <nav className='app-nav' style={!titleView ? {} : {display: 'none'}}>
-                <span className='app-nav-title'>Mike Mayo</span>
+            <nav className='app-nav'> 
+                <Logo
+                site='linkedin'
+                inView={titleView}
+                styles={{margin: '.75rem 1.5rem', position: 'absolute'}}
+                nav={true} />
+
+                <span className={`app-nav-title app-nav-slide-${titleView ? 'out' : 'in'}`}>
+                    Mike Mayo
+                </span>
+
+                <Logo
+                site='github'
+                inView={titleView}
+                styles={{margin: '.75rem 1.5rem', position: 'absolute'}} 
+                nav={true}/>
             </nav>
+
             <header className='app-header' ref={titleRef}>
                 <span
                 className='app-myName' style={titleView ? {} : {display: 'none'}}>
                     Mike Mayo
                 </span>
                 
-                <div className={`app-descriptor ${shake ? 'shake' : ''}`}>
+                <div className={`app-descriptor ${shake ? 'shake' : ''} ${shook ? 'shook' : ''}`}>
                     <span
-                    className={`app-descriptor-thinker app-descriptor ${!thinkerView ? 'outview' : '' } ${titleView ? '' : 'hidden'}`}>
+                    className={`app-descriptor-thinker app-descriptor ${!thinkerView ? 'outview' : titleView ? '' : 'hidden'}`}>
                         Thinker
                     </span>
 
