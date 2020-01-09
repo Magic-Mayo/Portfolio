21c6b2d7b0fe4da1034bce9a7851cf73394e8fd7
Magic-Mayo <mike.mayoii@gmail.com>
2020-01-09 16:45:44 -0700

    added onScreen hook to pass into other components

--- src/page/Portfolio.js
+++ src/page/Portfolio.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import Header from '../components/Header'
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 import projectData from '../data/projects.json'
+import useOnScreen from '../hooks';
 
 export default function Portfolio(){
+
+    const [aboutRef, aboutView] = useOnScreen({rootMargin: '-600px 0px -300px 0px'});
+    const [contactRef, contactView] = useOnScreen({threshold: .5});
+    const [projectRef, projectView] = useOnScreen({rootMargin: '-100px 0px 200px 0px'});
+
     return (
         <>
-            <About />
-            <section className='project-wrapper'>
-                <h1 className='project-header'>Projects</h1>
+            <Header />
+            <About
+            aboutRef={aboutRef}
+            aboutView={aboutView}
+            />
+            <span className={`project${projectView ? '-to-nav' : '-off'}`}>Projects</span>
+            <h1 className={`project-header${!projectView ? '' : '-to-nav'}`}>Projects</h1>
+            <section className='project-wrapper' ref={projectRef}>
                 <span className='project-container'>
                     {projectData[0].map(project=>{
                         return <Projects
@@ -50,7 +62,9 @@ export default function Portfolio(){
                     })}
                 </span>
             </section>
-            <Contact />
+            <Contact
+            contactRef={contactRef}
+            contactView={contactView} />
         </>
     )
 }
