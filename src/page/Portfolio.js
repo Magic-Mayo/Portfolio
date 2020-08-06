ce0665e7846cbf53a55a16689f6967ba1a140c7b
Magic-Mayo <mike.mayoii@gmail.com>
2020-08-05 22:43:09 -0700

    added routes and links in each route. conditional links based on window dimensions

--- src/page/Portfolio.js
+++ src/page/Portfolio.js
@@ -1,81 +1,115 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Nav from '../components/Nav';
 import Header from '../components/Header'
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 import projectData from '../data/projects.json'
-import useOnScreen from '../hooks';
+import { Route, Link } from 'react-router-dom';
+import Arrow from '../components/Logos/arrow';
+import DownArrow from '../components/Logos/downArrow';
+import useWindowDimensions from '../hooks/useWindowDimensions';
 
 export default function Portfolio(){
-
-    const [headerRef, headerOnScreen] = useOnScreen({threshold: .3}, true);
-    const [projectRef, projectOnScreen] = useOnScreen({threshold: .15});
-    const [aboutRef, aboutOnScreen] = useOnScreen({threshold: .4});
-    const [contactRef, contactOnScreen] = useOnScreen({threshold: .5});
+    const [showProject, setShowProject] = useState(0);
+    const {innerWidth, innerHeight} = useWindowDimensions();
 
     return (
         <>
-            <Header
-            headerOnScreen={headerOnScreen}
-            headerRef={headerRef}
-            aboutOnScreen={aboutOnScreen}
-            projectOnScreen={projectOnScreen}
-            contactOnScreen={contactOnScreen}
-            />
-            <span className={`project-nav project-nav${projectOnScreen && !headerOnScreen ? '-to' : ''}`}>Projects</span>
-            <section className={`project-wrapper ${headerOnScreen || aboutOnScreen ? 'transparent' : 'opaque'}`} ref={projectRef}>
-                <span className='project-container'>
-                    {projectData[0].map(project=>{
-                        return <Projects
-                        key={project.name}
-                        name={project.name}
-                        address={project.address}
-                        img={project.img}
-                        gif={project.gif}
-                        github={project.github}
-                        desc={project.description}
-                        />
-                    })}
-                </span>
-                <span className='project-container'>
-                    {projectData[1].map(project=>{
-                        return <Projects
-                        key={project.name}
-                        name={project.name}
-                        address={project.address}
-                        img={project.img}
-                        gif={project.gif}
-                        github={project.github}
-                        desc={project.description}
-                        />
-                    })}
-                </span>
-                <span className='project-container'>
-                    {projectData[2].map(project=>{
-                        return <Projects
-                        key={project.name}
-                        name={project.name}
-                        address={project.address}
-                        img={project.img}
-                        gif={project.gif}
-                        github={project.github}
-                        desc={project.description}
-                        />
-                    })}
-                </span>
-            </section>
-            <About
-            aboutRef={aboutRef}
-            aboutOnScreen={aboutOnScreen}
-            contactOnScreen={contactOnScreen}
-            projectOnScreen={projectOnScreen}
+            <Nav
+            mobile={innerWidth < 675 || innerHeight < 700}
             />
 
-            <Contact
-            contactRef={contactRef}
-            contactOnScreen={contactOnScreen}
-            aboutOnScreen={aboutOnScreen}
-            />
+            <Route exact path='/'>
+                <Header />
+                {innerWidth > 675 &&
+                    <Link to='/projects' className='link-arrow'>
+                        <DownArrow>
+                            <span>Projects</span>
+                        </DownArrow>
+                    </Link>
+                }
+            </Route>
+
+            <Route path='/projects'>
+                <section
+                className='project-wrapper'
+                >
+                    <span
+                    onClick={() => setShowProject(prevProj => prevProj > 0 ? prevProj - 1 : projectData.length - 1)}
+                    className='arrow prev-arrow'
+                    >
+                        <Arrow />
+                    </span>
+
+                    {projectData.map((project, ind) => {
+                        return <Projects
+                        key={ind}
+                        showProject={showProject === ind}
+                        {...project}
+                        />
+                    })}
+
+                    <span
+                    onClick={() => setShowProject(prevProj => prevProj === projectData.length - 1 ? 0 : prevProj + 1)}
+                    className='arrow next-arrow'
+                    >
+                        <Arrow />
+                    </span>
+
+                    {innerWidth > 675 &&
+                        <Link to='/about' className='link-arrow'>
+                            <DownArrow>
+                                <span>About</span>
+                            </DownArrow>
+                        </Link>
+                    }
+                </section>
+            </Route>
+
+            <Route path='/about'>
+                {innerWidth > 675 &&
+                    <Link to='/projects' className='up-arrow link-arrow'>
+                        <DownArrow
+                        top={true}
+                        >
+                            <span>Projects</span>
+                        </DownArrow>
+                    </Link>
+                }
+
+                <About />
+
+                {innerWidth > 675 &&
+                    <Link to='/contact' className='link-arrow'>
+                        <DownArrow>
+                            <span>Contact</span>
+                        </DownArrow>
+                    </Link>
+                }
+            </Route>
+
+            <Route path='/contact'>
+                {innerWidth > 675 &&
+                    <Link to='/about' className='up-arrow link-arrow'>
+                        <DownArrow
+                        top={true}
+                        >
+                            <span>About</span>
+                        </DownArrow>
+                    </Link>
+                }
+                
+                <Contact />
+            </Route>
+            <div style={{display: 'none'}}>
+                Icons made by
+                <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">
+                    Pixel perfect 
+                </a>
+                from 
+                <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+            </div>
         </>
     )
 }
