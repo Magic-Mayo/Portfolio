b15ea0d668175d782123a04cb2b0ad1d29407189
Magic-Mayo <mike.mayoii@gmail.com>
2020-08-07 00:23:15 -0700

    passed in props for class names and onclick function

--- src/components/Logos/downArrow.js
+++ src/components/Logos/downArrow.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
-export default ({children, top}) => {
+export default ({children, top, className, onClick}) => {
     return (
-        <span className='down-arrow'>
-            {top && children}
-            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 490.656 490.656">
-            <g>
-            <path d="M487.536,120.445c-4.16-4.16-10.923-4.16-15.083,0L245.317,347.581L18.203,120.445c-4.16-4.16-10.923-4.16-15.083,0
-                c-4.16,4.16-4.16,10.923,0,15.083l234.667,234.667c2.069,2.091,4.8,3.136,7.531,3.136s5.461-1.045,7.552-3.115l234.667-234.667
-                C491.696,131.368,491.696,124.627,487.536,120.445z"/>
-            </g>
+        <span
+        className={className ? className : 'down-arrow'}
+        onClick={onClick}
+        >
+            {top && !className && children}
+            <svg viewBox="0 0 490.656 490.656">
+                <g>
+                    <path d="M487.536,120.445c-4.16-4.16-10.923-4.16-15.083,0L245.317,347.581L18.203,120.445c-4.16-4.16-10.923-4.16-15.083,0
+                        c-4.16,4.16-4.16,10.923,0,15.083l234.667,234.667c2.069,2.091,4.8,3.136,7.531,3.136s5.461-1.045,7.552-3.115l234.667-234.667
+                        C491.696,131.368,491.696,124.627,487.536,120.445z"/>
+                </g>
             </svg>
-            {!top && children}
+            {!top && !className && children}
         </span>
     )
 }
