c9bf04a6813d8318b5d4076cc1711387ca0686d4
Magic-Mayo <mike.mayoii@gmail.com>
2020-08-09 13:09:09 -0700

    removed span and props

--- src/components/Logos/arrow.js
+++ src/components/Logos/arrow.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-export default ({className, onClick}) => <span className={className} onClick={onClick}>
-    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px"
+export default () => <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px"
 	viewBox="0 0 337.792 337.792">
         <g>
             <path d="M337.792,134.824l-130.824,99.441v-52.216C117.061,173.419,30.735,207.817,0.001,302.41
                 C-0.337,180.728,99.895,111.781,206.968,88.488V35.382L337.792,134.824z"/>
         </g>
     </svg>
-</span>
